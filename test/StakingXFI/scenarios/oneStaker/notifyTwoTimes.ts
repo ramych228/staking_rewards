60d7042c45dd06cc342de29c95eae954280d597c
SmartMark <0xsmartmark@gmail.com>
2023-10-16 00:45:23 +0800

    foud bug TWOOO

--- test/StakingXFI/scenarios/oneStaker/notifyTwoTimes.ts
+++ test/StakingXFI/scenarios/oneStaker/notifyTwoTimes.ts
@@ -4,6 +4,8 @@ import { getStakingContractWithStakers } from '../../_.fixtures'
 import { expect } from 'chai'
 import { sign } from 'crypto'
 
+// TODO: Check how accurately period finish changes during the time
+
 export const notifyTwoTimes = function () {
     it("notify 1 and after X secs notify 2", async function(){
         const { signers, staking, rewardToken, stakingToken, duration } = await getStakingContractWithStakers()
@@ -13,6 +15,8 @@ export const notifyTwoTimes = function () {
         const tokenStakeAmount = ethers.parseEther("0.1");
 
         // ----------- ACTION ---------------
+        console.log(ethers.formatEther(await rewardToken.balanceOf(staker)));
+
         await staking.connect(staker).stake(tokenStakeAmount);
         const periodFinish = (await time.latest()) + Number(duration);
         await staking.notifyTokenRewardAmount(tokenRewardAmount1);
@@ -20,17 +24,20 @@ export const notifyTwoTimes = function () {
         await time.increaseTo(await staking.tokenPeriodFinish() - ((duration / 2n) + 1n));
         await staking.connect(staker).getReward()
 
-        const balanceChange = ethers.parseEther("0.5");
+        const balanceChange = tokenRewardAmount1 / 2n;
         expect(await rewardToken.balanceOf(staker)).to.approximately(balanceChange, 100n);
         expect(await staking.tokenPeriodFinish()).to.approximately(periodFinish, 5n);
 
-        console.log(await rewardToken.balanceOf(staking));
+        console.log(ethers.formatEther(await rewardToken.balanceOf(staker)));
         await staking.notifyTokenRewardAmount(tokenRewardAmount2);
-        expect(await staking.tokenPeriodFinish()).to.equal(periodFinish + Number(duration));
+        expect(await staking.tokenPeriodFinish()).to.approximately(periodFinish + Number(duration), 30n);
 
         await time.increase(await staking.tokenPeriodFinish() + 1n);
-
-        const balanceChange2 = ethers.parseEther("3.5"); 
+        console.log("the staking contract balance is %s", ethers.formatEther(await rewardToken.balanceOf(staking)));
+        await staking.connect(staker).getReward();
+        
+        console.log(ethers.formatEther(await rewardToken.balanceOf(staker)));
+        const balanceChange2 = tokenRewardAmount1 + tokenRewardAmount2; 
         expect(await rewardToken.balanceOf(staker)).to.approximately(balanceChange2, 100n);
 
     })
