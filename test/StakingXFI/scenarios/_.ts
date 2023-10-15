1e0d5e33fd62e649efffc07aef526dc4607198f8
0xShaka <0xShaka@gmail.com>
2023-10-15 21:38:52 +0300

    Overflow in 1 period in getReward()

--- test/StakingXFI/scenarios/_.ts
+++ test/StakingXFI/scenarios/_.ts
@@ -2,15 +2,14 @@ import { complexScenario } from './complexScenario'
 import { decreasingStakes } from './decreasingStakes'
 import { increasingStakes } from './increasingStakes'
 import { lotsOfStakers } from './lotsOfStakers'
-import { oneStaker} from './oneStaker/_'
+import { oneStaker } from './oneStaker/_'
 
 export const scenarios = function () {
-
 	/* --- one staker --- */
 
 	describe('one staker', oneStaker)
 
-	it.skip('complex scenario', complexScenario)
+	it.only('complex scenario', complexScenario)
 
 	it.skip('decreasing stakes', decreasingStakes)
 	it.skip('increasing stakes', increasingStakes)
