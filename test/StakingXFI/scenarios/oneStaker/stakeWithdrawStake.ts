957d7df8d4e9f130d943f9bf4caea8ec3d65351e
SmartMark <0xsmartmark@gmail.com>
2023-10-16 21:42:43 +0800

    found bug THREE?

--- test/StakingXFI/scenarios/oneStaker/stakeWithdrawStake.ts
+++ test/StakingXFI/scenarios/oneStaker/stakeWithdrawStake.ts
@@ -91,4 +91,37 @@ export const stakeWithdrawStake = async function () {
 
         expect(await rewardToken.balanceOf(staker)).to.approximately(tokenRewardAmount / 2n, 100n);
     })
+
+
+    it.only("stake before notify, withdraw X tokens in the middle of reward duration (NativeNotify)", async function () {
+        const { signers, staking, rewardToken, stakingToken, duration } = await getStakingContractWithStakers()
+        const staker = signers[1];
+        const stakeAmount = ethers.parseEther("10");
+        const tokenRewardAmount = ethers.parseEther("500");
+        
+        // ----------- ACTION ---------------
+
+        // FIRST STAKE
+        await staking.connect(staker).stake(stakeAmount);
+        console.log("hey");
+        await staking.notifyNativeRewardAmount(tokenRewardAmount, {value: tokenRewardAmount});
+        console.log("hey2");
+        await time.increaseTo(await staking.nativePeriodFinish() - ((duration / 2n) + 1n));
+
+        const balanceChange = tokenRewardAmount / 2n;
+        // await expect(staking.connect(staker).getReward()).to.changeEtherBalances([staker, staking], [balanceChange, -balanceChange]);
+        
+        // WITHDRAWING COUPLE TIMES OF THE STAKED BALANCE & INC. TIME TO DURATION END
+        for (let i = 0; i < 10; i++) { // was 100
+            await staking.connect(staker).withdraw(stakeAmount / 200n);
+            // console.log(await staking.balanceLPOf(staker));
+        }
+
+        await time.increaseTo(await staking.nativePeriodFinish() + 1000000n);
+        // await staking.connect(staker).getReward();
+
+        await staking.connect(staker).getReward();
+        console.log(ethers.formatEther(await staking.balanceSTOf(staker)));
+        // expect(await ethers.provider.getBalance(staker)).to.approximately(tokenRewardAmount, 100n);
+    })
 }
