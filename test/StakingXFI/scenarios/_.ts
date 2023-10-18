a32bcacbbce500f4992e7d5375623a8e53886838
SmartMark <0xsmartmark@gmail.com>
2023-10-18 20:36:58 +0800

    minor changes

--- test/StakingXFI/scenarios/_.ts
+++ test/StakingXFI/scenarios/_.ts
@@ -2,10 +2,9 @@ import { complexScenario } from './complexScenario'
 import { decreasingStakes } from './decreasingStakes'
 import { increasingStakes } from './increasingStakes'
 import { lotsOfStakers } from './lotsOfStakers'
-import { oneStaker} from './oneStaker/_'
+import { oneStaker } from './oneStaker/_'
 
 export const scenarios = function () {
-
 	/* --- one staker --- */
 
 	describe('one staker', oneStaker)
