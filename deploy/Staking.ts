d6c0f2666e50ab0f516461da3c78272177c1c51f
0xShaka <0xShaka@gmail.com>
2023-11-17 22:50:07 +0300

    Fixed deployment script

--- deploy/Staking.ts
+++ deploy/Staking.ts
@@ -18,3 +18,10 @@ async function main() {
 
 	console.log('Staking deployed at:', await staking.getAddress())
 }
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.log(error)
+		process.exit(1)
+	})
